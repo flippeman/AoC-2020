5c94aeb108a76fc70f32895a5b5765354ec7af02
Filip Andréasson <filandr@student.chalmers.se>
2020-12-08 11:59:08 +0100

    Day 7 completed!

--- 7/7.js
+++ 7/7.js
@@ -1,30 +1,44 @@
 var fs = require('fs');
 var rules = fs.readFileSync('7/input.txt').toString().split("\r\n").map(rule => rule.split(/contain|,/gm));
 
-//console.log(rules);
-var temp = "Hej jag heter bajs";
-console.log(temp.slice(0,temp.indexOf("bajs")));
-console.log(["1","2"].concat((["3","4"]).concat(["5","6"])));
-console.log(part1());
-//console.log(part2());
+console.log(part1().length);
+console.log(part2());
 
 
 function part1() {
-  return canCarry("shiny gold");
+  return [...new Set(canCarry("shiny gold"))];
 }
+
 
 function canCarry(parentBag) {
   var tempArray = [];
   rules.filter(rule => rule.slice(1).some(bag => {
     if (bag.includes(parentBag)) tempArray.push(rule[0].slice(0,rule[0].indexOf(" bags")));
   }));
-  console.log(tempArray);
-  return tempArray.concat();
+  for (var bag in tempArray) {
+    tempArray = tempArray.concat(canCarry(tempArray[bag]));
+  }
+  return tempArray;
 }
 
-/*{
-  return (bag.includes("shiny gold") ? [parentBag] : )
-})*/
-function part2() {
 
+function part2() {
+  return insideBag("shiny gold",1)
+}
+
+
+function insideBag(parentBag, scalar) {
+  var returnVal = 0;
+  rules.filter(rule => rule[0].includes(parentBag))[0].slice(1).forEach(bag => {
+      if (!bag.includes("no other bags")) {
+        returnVal += parseInt(insideBag(bag.trim().substring(2,bag.indexOf(" bag")),bag.trim().charAt(0)));
+      }
+    });
+  if (parentBag == "shiny gold") {
+      return returnVal;
+  } else if (returnVal > 0) {
+    return (parseInt(returnVal) * parseInt(scalar))+parseInt(scalar);
+  } else {
+    return parseInt(scalar);
+  }
 }
